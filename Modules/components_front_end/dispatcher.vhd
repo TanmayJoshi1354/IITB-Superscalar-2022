a3d4f4a8d221e93813047eb58dbad08090a26292
TanmayJoshi1354 <81479982+TanmayJoshi1354@users.noreply.github.com>
2022-12-08 19:33:05 +0530

    dispatch buffer updated

--- Modules/components_front_end/dispatcher.vhd
+++ Modules/components_front_end/dispatcher.vhd
@@ -12,14 +12,15 @@ architecture dispatch of dispatcher is
 shared variable counter : integer range 0 to 99 :=0;
 signal I1N, I2N: std_logic_vector(6 downto 0);
 begin
-process_disp: process(instruction1, instruction2)
+process_disp: process(instruction1, instruction2,clock)
 begin
+if(clock'event and clock = '1') then
 outstruction1(53 downto 47) <= std_logic_vector(to_unsigned(counter,7));
 counter := counter +1;
 outstruction1(46 downto 0) <= instruction1;
 outstruction2(53 downto 47) <= std_logic_vector(to_unsigned(counter,7));
 counter := counter +1;
 outstruction2(46 downto 0) <= instruction2;
-
+end if;
 end process;
 end architecture dispatch;
